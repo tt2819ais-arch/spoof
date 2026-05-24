142b7e98425785b1629cd71826ba987f18537dda
tt2819ais-arch <tt2819ais@gmail.com>
2026-05-24 23:18:17 +0300

    Update SpoofMode.kt

--- data/model/SpoofMode.kt
+++ data/model/SpoofMode.kt
@@ -1,1 +1,16 @@
+package com.cameraspoof.data.model
 
+enum class SpoofMode(val displayName: String, val description: String) {
+    STATIC_JPEG(
+        "Static JPEG",
+        "Direct JPEG injection without modification"
+    ),
+    REGENERATED(
+        "Regenerated Image",
+        "New EXIF metadata, timestamp and random filename"
+    ),
+    PSEUDO_LIVE(
+        "Pseudo Live Preview",
+        "1-2 second fake camera preview before returning image"
+    )
+}
